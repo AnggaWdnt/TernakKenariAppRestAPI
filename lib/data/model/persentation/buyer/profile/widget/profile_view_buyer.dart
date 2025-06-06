0bd43d55bd62225c7e3554df00aa93feb9f34cc8
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-06-07 05:00:50 +0700

    tambah code widget profileviewbuyer

---
+++ lib/data/model/persentation/buyer/profile/widget/profile_view_buyer.dart
@@ -0,0 +1,8 @@
+import 'package:canary_template/core/core.dart';
+import 'package:flutter/material.dart';
+
+class ProfileViewBuyer extends StatelessWidget {
+  final Data profile;
+  const ProfileViewBuyer({super.key, required this.profile});
+
+}
