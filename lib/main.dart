89c2dd16014229e099f9686e2c8bc0f0d1fd547d
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-06-07 05:49:33 +0700

    tambahan code pada main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:canary_template/data/repository/auth_repository.dart';
+import 'package:canary_template/data/repository/get_all_burung_tersedia_repository.dart';
+import 'package:canary_template/data/repository/profile_buyer_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:canary_template/data/repository/auth_repository.dart';
+import 'package:canary_template/data/repository/get_all_burung_tersedia_repository.dart';
+import 'package:canary_template/data/repository/profile_buyer_repository.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -10,27 +18,42 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create:
+              (context) => LoginBloc(
+                authRepository: AuthRepository(ServiceHttpClient()),
+              ),
+        ),
+        BlocProvider(
+          create:
+              (context) => RegisterBloc(
+                authRepository: AuthRepository(ServiceHttpClient()),
+              ),
+        ),
+        BlocProvider(
+          create:
+              (context) => ProfileBuyerBloc(
+                profileBuyerRepository: ProfileBuyerRepository(
+                  ServiceHttpClient(),
+                ),
+              ),
+        ),
+        BlocProvider(
+          create:
+              (context) => GetBurungTersediBloc(
+                GetAllBurungTersediaRepository(ServiceHttpClient()),
+              ),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: const Splashscreen(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
