3077ed2887fdc1196d0715d888a012f3c1bb70a7
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-06-06 22:22:38 +0700

    tambah deklarasi pada induk request model

---
+++ lib/data/model/request/admin/induk_request_model.dart
@@ -0,0 +1,11 @@
+import 'dart:convert';
+
+class IndukRequestModel {
+  final String? noRing;
+  final DateTime? tanggalLahir;
+  final String? jenisKelamin;
+  final String? jenisKenari;
+  final String? keterangan;
+  final String? gambarInduk;
+
+}
