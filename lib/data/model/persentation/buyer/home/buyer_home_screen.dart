2611071db23f6d118a5ca6a4f0da13a33b1601fe
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-06-07 05:30:02 +0700

    tambah inistate

--- lib/data/model/persentation/buyer/home/buyer_home_screen.dart
+++ lib/data/model/persentation/buyer/home/buyer_home_screen.dart
@@ -1,5 +1,14 @@
+import 'package:canary_template/data/model/persentation/bloc/get_burung_tersedia/get_burung_tersedia_bloc.dart';
+import 'package:canary_template/data/model/persentation/bloc/get_burung_tersedia/get_burung_tersedia_event.dart';
 import 'package:flutter/material.dart';
 
 class BuyerHomeScreen extends StatefulWidget {
   const BuyerHomeScreen({super.key});
+
+  @override
+void initState() {
+  super.initState ();
+  context.read<GetBurungTersediBloc>().add(GetAllBurungTersediaEvent());
+}
+
 }
