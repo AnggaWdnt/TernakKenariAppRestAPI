eb3adae8b916b17ca92a1cb4e307f45daea907c6
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-06-07 05:16:07 +0700

    tambah code inistate trigger

---
+++ lib/data/model/persentation/buyer/profile/buyer_profile_screen.dart
@@ -0,0 +1,18 @@
+import 'package:canary_template/data/model/persentation/buyer/profile/bloc/profile_buyer_bloc.dart';
+import 'package:canary_template/data/model/persentation/buyer/profile/bloc/profile_buyer_event.dart';
+import 'package:flutter/material.dart';
+
+class BuyerProfileScreen extends StatefulWidget {
+  const BuyerProfileScreen({super.key});
+
+  @override
+  State<BuyerProfileScreen> createState() => _BuyerProfileScreenState();
+}
+
+class _BuyerProfileScreenState extends State<BuyerProfileScreen> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<ProfileBuyerBloc>().add(GetProfileBuyerEvent());
+  }
+}
