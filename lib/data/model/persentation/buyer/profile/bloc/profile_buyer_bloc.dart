ffdc091ecf6c9577e143e9123feb14b739c5f333
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-06-07 04:53:33 +0700

    tambah code constructor

--- lib/data/model/persentation/buyer/profile/bloc/profile_buyer_bloc.dart
+++ lib/data/model/persentation/buyer/profile/bloc/profile_buyer_bloc.dart
@@ -1,4 +1,10 @@
 class ProfileBuyerBloc extends Bloc<ProfileBuyerEvent, ProfileBuyerState> {
   final ProfileBuyerRepository profileBuyerRepository;
+  ProfileBuyerBloc({required this.profileBuyerRepository})
+    : super(ProfileBuyerInitial()) {
+    on<AddProfileBuyerEvent>(_addProfileBuyer);
+    on<GetProfileBuyerEvent>(_getProfileBuyer);
+  }
+
 
 }
