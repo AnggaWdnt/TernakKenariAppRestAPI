b5217c2ff40db2722a4cc5163ce3fbeb73ec0fe0
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-06-06 22:43:32 +0700

    perbaikan import induk respository

--- lib/data/repository/induk_repository.dart
+++ lib/data/repository/induk_repository.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:canary_template/data/model/request/admin/induk_request_model.dart';
 import 'package:canary_template/service/service_http_client.dart';
 import 'package:dartz/dartz.dart';
 
